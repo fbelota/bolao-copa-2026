4a175008a7347a01b2f13f447bb45ff6679fc1fb
fbelota <75803420+fbelota@users.noreply.github.com>
2026-06-20 16:51:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.BOLAO_CONFIG = {
   SUPABASE_URL: "https://ppzpfhnnfjplagfozyuh.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_VxyGmUjNndDKvyn5_tEipw_ej8qh888",
-  VALOR_PALPITE: 10,
+  VALOR_PALPITE: 5,
   PIX_CHAVE: "COLE_AQUI_SUA_CHAVE_PIX",
-  PIX_TEXTO: "Pix: COLE_AQUI_SUA_CHAVE_PIX | Valor: R$ 10,00",
-  WHATSAPP_ORGANIZADOR: "5592999999999"
+  PIX_TEXTO: "Chave Pix (CPF): 58804978287 | Valor: R$ 5,00",
+  WHATSAPP_ORGANIZADOR: "5592984728319"
 };
